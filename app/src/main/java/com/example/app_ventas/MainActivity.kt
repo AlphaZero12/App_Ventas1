bc504f956aa14447fed476bc8a4e2adf8af24774
Sergio <sergiotgp747@gmail.com>
2023-09-28 16:33:41 -0300

    Segunda version app1

--- app/src/main/java/com/example/app_ventas/MainActivity.kt
+++ app/src/main/java/com/example/app_ventas/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+public  class MainActivity : AppCompatActivity()
+{
+
     private lateinit var BotonLogin: Button
     private lateinit var BotonRegistro: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
 
@@ -25,12 +29,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
         BotonRegistro.setOnClickListener {
             // Abrir la actividad de registro
             val intent = Intent(this, ActivityRegistro::class.java)
             startActivity(intent)
             setContentView(R.layout.activity_registro)
         }
+
     }
+
 }
+
 
